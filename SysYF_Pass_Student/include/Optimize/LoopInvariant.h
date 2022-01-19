71413aabe7cb24fad1be2315a21cd8139b04a6ed
DumbAnonymous <andyxue@mail.ustc.edu.cn>
2022-01-19 22:11:27 +0800

    LoopInvariant & ConstPropagation, xdy

--- SysYF_Pass_Student/include/Optimize/LoopInvariant.h
+++ SysYF_Pass_Student/include/Optimize/LoopInvariant.h
@@ -7,17 +7,22 @@
 #include <map>
 #include <stack>
 #include <set>
+#include "CFG_analyse.h"
 #include <memory>
 
 class LoopInvariant : public Pass
 {
+private:
+    /* data */
+    std::unique_ptr<CFG_analyse> CFG_analyser;
+    std::vector<std::pair<BasicBlock *, std::set<Instruction *>>> invariants;
+    std::string name = "LoopInvariant";
+
 public:
     explicit LoopInvariant(Module* module): Pass(module){}
     void execute() final;
+    void invariants_find(std::vector<BasicBlock *>* );
     const std::string get_name() const override {return name;}
-
-private:
-    std::string name = "LoopInvariant";
 };
 
 
